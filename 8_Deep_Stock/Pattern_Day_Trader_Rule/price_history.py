82385730915a75ee74d117aa58862149b42e9ee6
The Bear @ Austin <46735920+zebointexas@users.noreply.github.com>
2020-05-22 12:01:15 -0500

    Update price_history.py

--- 8_Deep_Stock/Pattern_Day_Trader_Rule/price_history.py
+++ 8_Deep_Stock/Pattern_Day_Trader_Rule/price_history.py
@@ -19,7 +19,7 @@ while True:
     time.sleep(30)
     print('Now -->  ' + str(datetime.datetime.now()))
     if(in_time_range('0830-1600')):
-        robin_stocks.login('zeboxiong.texas@gmail.com', 'XKSJSG@Kuhgd98173~&%$#')
+        robin_stocks.login('-----', '-------')
         cur_Price = robin_stocks.stocks.get_latest_price('SAVE', True)
         csvdata = [datetime.datetime.now()]
         csvFile = open("2020_0521_SAVE.csv", "a")
